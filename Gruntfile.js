0dc934f43286c5ab7fe2ed333aa9bd3b225a0d8f
Geison Santos <geison.sn@gmail.com>
2016-12-18 13:37:34 -0400

    Adição de suporte a minificação de imagens com o grunt-contrib-imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,17 +19,26 @@ module.exports = function(grunt) {
         },
         usemin: {
             html: 'dist/**/*.html'
+        },
+        imagemin: {
+            public: {
+                expand: true,
+                cwd: 'dist/img',
+                src: '**/*.{png, jpg, gif}',
+                dest: 'dist/img'
+            }
         }
     });
 
     grunt.registerTask('dist', ['clean', 'copy']);
-    grunt.registerTask('minifica', ['useminPrepare', 'concat:generated', 'uglify:generated', 'cssmin:generated', 'usemin']);
-    grunt.registerTask('default', ['dist', 'minifica', 'clean:nonDist']);
+    grunt.registerTask('min', ['useminPrepare', 'concat:generated', 'uglify:generated', 'cssmin:generated', 'usemin', 'imagemin']);
+    grunt.registerTask('default', ['dist', 'min', 'clean:nonDist']);
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-usemin');
 }
